e89f18bccecf8bcb0fb4b0c8acf9e9b3802fb677
durandneto <durand.neto@gmail.com>
2022-03-14 22:41:22 -0400

    1 file modified, 1 added

--- index.tsx
+++ index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-import Users from './useState/Users';
+import PageTittle from './useEffect/PageTittle';
 
 const App = () => {
   return (
     <div>
       <h1>Welcome to Learning with Turing</h1>
-      <h2>ReactJS useState</h2>
-      <Users />
+      <h2>ReactJS useEffect</h2>
+      <PageTittle />
     </div>
   );
 };
---
+++ useEffect/PageTittle.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+export default function PageTittle() {
+  useEffect(() => {
+    // after de component load
+    // I can use Jquey here :)
+    // I can connect to webserver here
+  });
+
+  return (
+    <div>
+      <p>You clickedtimes</p>
+    </div>
+  );
+}
